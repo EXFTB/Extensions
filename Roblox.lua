9a0b23e70ec8af5254deb9441fd14c87bb8a1480
Legacy <100034435+EXFTB@users.noreply.github.com>
2023-09-11 19:11:05 +0200

    Update Roblox.lua

--- Roblox.lua
+++ Roblox.lua
@@ -3,6 +3,7 @@ if getfenv()["hookfunction"] == nil or getfenv()["hookmetamethod"] == nil then r
 local Extension = {
     Variables = {},
     Functions = {},
+    Cache = {},
 }
 getgenv().Extension = Extension
 
@@ -78,7 +79,7 @@ end
 
 function Extension.Functions.HookMethod(Table: any, Method: any, NewFunction: any, Example: boolean)
     if Example ~= nil and Example == true then
-        print("Example: \nMethod = anything that is being called with parentheses such as object:Destroy() or remoteevent:FireServer() or object:GetAttribute()\n\nCaller = The script calling the function\n\ncheckcaller() = returns true if the executor is the caller\n\nTip: When using __namecall hooks, please use . instead of :, such as STR:find(partofstring) would be STR.find(STR, partofstring) or object:GetAttribute(att) would be object.GetAttribute(object, att) or object:Destroy() would be object.Destroy(object) \n\nlocal OldFunction = nil\nOldFunction = Extension.Functions.HookMethod(game, \"__namecall\", function(self, ...)\n    local Arguements = {...}\n    local NcallSelf = tostring(self)\n    local Caller = getcallingscript()\n    local Method = getnamecallmethod()\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" then\n        return --If anything on the CLIENT tries to destroy any humanoid\n    end\n\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If anything on the CLIENT tries to destroy your humanoid\n    end\n\n\n    if (not checkcaller()) and Method == \"Destroy\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If the game (CLIENT) tries to destroy anything in your character\n    end    \n    return OldFunction(self, ...)\nend)")
+        print("Example: \nMethod = anything that is being called with parentheses such as object:Destroy() or remoteevent:FireServer() or object:GetAttribute()\n\nCaller = The script calling the function\n\ncheckcaller() = returns true if the executor is the caller\n\nTip: When using __namecall hooks, please use . instead of : to avoid C-Stack overflow, such as STR:find(partofstring) would be STR.find(STR, partofstring) or object:GetAttribute(att) would be object.GetAttribute(object, att) or object:Destroy() would be object.Destroy(object) \n\nlocal OldFunction = nil\nOldFunction = Extension.Functions.HookMethod(game, \"__namecall\", function(self, ...)\n    local Arguements = {...}\n    local NcallSelf = tostring(self)\n    local Caller = getcallingscript()\n    local Method = getnamecallmethod()\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" then\n        return --If anything on the CLIENT tries to destroy any humanoid\n    end\n\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If anything on the CLIENT tries to destroy your humanoid\n    end\n\n\n    if (not checkcaller()) and Method == \"Destroy\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If the game (CLIENT) tries to destroy anything in your character\n    end    \n    return OldFunction(self, ...)\nend)")
     else
         if Table ~= nil and Method ~= nil and tostring(Method) ~= nil and Method:find("__") and NewFunction ~= nil and typeof(NewFunction) == "function" then
             local GetMetatable = getrawmetatable or debug.getmetatable
@@ -102,6 +103,32 @@ function Extension.Functions.HookMethod(Table: any, Method: any, NewFunction: an
         else
             warn("Error, possible errors:\nTable is nil\nMethod is nil\nMethod is invalid\nNewFunction is nil\nNewFunction is invalid, please refer to the example.")
         end
+    end
+end
+
+function Extension.Functions.CreateProxy(Target: any, Example: boolean)
+    if Example ~= nil and Example == true then
+        print("Example: \nAs mentioned in the example when using a hookmetamethod with namecall, you have to replace ':' functions with '.' to avoid C-Stack overflow, this will do so automatically\nExtension.Functions.CreateProxy(self)\nself:IsA(\"BasePart\") will now automatically read as self.IsA(self, \"BasePart\")")
+    elseif Target ~= nil and (typeof(Target) == "Instance" or typeof(Target) == "string") then
+        if Extension.Cache[Target] then
+            return Extension.Cache[Target]
+        end
+
+        local function Proxy(self, ...)
+            local Modified = setmetatable({}, {
+                __index = function(_, Method)
+                    return function(_, ...)
+                        return Target[Method](Target, self, ...)
+                    end
+                end,
+            })
+            return Modified
+        end
+
+        Extension.Cache[Target] = Proxy(Target) 
+        return Extension.Cache[Target]
+    else
+        warn("Error, possible errors:\nTarget is nil\nTarget is invalid, please refer to the example.")
     end
 end
 
