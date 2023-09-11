4a0b3d6ba794ea3a82a92290dc97f4fa7c1e1763
Legacy <100034435+EXFTB@users.noreply.github.com>
2023-09-11 21:35:17 +0200

    Update Roblox.lua

--- Roblox.lua
+++ Roblox.lua
@@ -1,23 +1,12 @@
-if getfenv()["hookfunction"] == nil or getfenv()["hookmetamethod"] == nil then return warn("Executor will not run all functions required for this extension") end
+if getfenv()["hookfunction"] == nil or getfenv()["hookmetamethod"] == nil or (getfenv()["getrawmetatable"] == nil and getfenv()["debug"] == nil) then return warn("Executor will not run all functions required for this extension") end
 
+local GetMetatable = getrawmetatable or debug.getmetatable
 local Extension = {
     Variables = {},
     Functions = {},
     Cache = {},
 }
 getgenv().Extension = Extension
-
-function Extension.Functions.Help(State)
-    if (not State) then
-        print("Please define state; Extension.Functions.Help(\"Example\") or Extension.Functions.Help(\"General\")")
-    elseif State and tostring(State) ~= nil then
-        if State == "Example" then
-            print("To know how to use each and every function that this extension, follow these steps:\nExample: Extensions.Functions.Alive(nil, nil, true), third arguement in this case being set to true will print out instructions on how to use the function.")
-        elseif State == "General" then
-            print("Arguements to all functions and all functions are listed in https://raw.githubusercontent.com/EXFTB/Extensions/main/Roblox.readme")
-        end
-    end
-end
 
 function Extension.Functions.Define(Name: string, Definition: any, Example: boolean)
     if Example ~= nil and Example == true then
@@ -82,13 +71,12 @@ function Extension.Functions.HookMethod(Table: any, Method: any, NewFunction: an
         print("Example: \nMethod = anything that is being called with parentheses such as object:Destroy() or remoteevent:FireServer() or object:GetAttribute()\n\nCaller = The script calling the function\n\ncheckcaller() = returns true if the executor is the caller\n\nTip: When using __namecall hooks, please use . instead of : to avoid C-Stack overflow, such as STR:find(partofstring) would be STR.find(STR, partofstring) or object:GetAttribute(att) would be object.GetAttribute(object, att) or object:Destroy() would be object.Destroy(object) \n\nlocal OldFunction = nil\nOldFunction = Extension.Functions.HookMethod(game, \"__namecall\", function(self, ...)\n    local Arguements = {...}\n    local NcallSelf = tostring(self)\n    local Caller = getcallingscript()\n    local Method = getnamecallmethod()\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" then\n        return --If anything on the CLIENT tries to destroy any humanoid\n    end\n\n\n    if Method == \"Destroy\" and NcallSelf:lower() == \"humanoid\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If anything on the CLIENT tries to destroy your humanoid\n    end\n\n\n    if (not checkcaller()) and Method == \"Destroy\" and tostring(self.Parent) == tostring(LocalPlayer) then\n        return --If the game (CLIENT) tries to destroy anything in your character\n    end    \n    return OldFunction(self, ...)\nend)")
     else
         if Table ~= nil and Method ~= nil and tostring(Method) ~= nil and Method:find("__") and NewFunction ~= nil and typeof(NewFunction) == "function" then
-            local GetMetatable = getrawmetatable or debug.getmetatable
-            if (not GetMetatable) then return error("Executor has no getrawmetatable. You cannot use this function") end
-
             local Metatable = GetMetatable(Table)
             if (not Metatable) then return error("Table has no metatable. Inavlid arguement \"table\" #1") end
 
-            if isreadonly(Metatable) then setreadonly(Metatable, false) end
+            if isreadonly(Metatable) then 
+                setreadonly(Metatable, false) 
+            end
 
             if islclosure(NewFunction) then
                 NewFunction = newcclosure(NewFunction)
@@ -97,7 +85,9 @@ function Extension.Functions.HookMethod(Table: any, Method: any, NewFunction: an
             local OldHookedFunction = nil
             OldHookedFunction = hookfunction(Metatable[Method], NewFunction)
 
-            if (not isreadonly(Metatable)) then setreadonly(Metatable, true) end
+            if (not isreadonly(Metatable)) then 
+                setreadonly(Metatable, true) 
+            end
 
             return OldHookedFunction
         else
@@ -130,6 +120,10 @@ function Extension.Functions.CreateProxy(Target: any, Example: boolean)
     else
         warn("Error, possible errors:\nTarget is nil\nTarget is invalid, please refer to the example.")
     end
+end
+
+function Extension.Functions.ClearCache()
+    Extension.Cache = {}
 end
 
 return getgenv().Extension
